90c8b059eb855a269a548c5d07297f2a30f25301
Musharaf <shahilhaque24@gmail.com>
2026-01-18 18:36:13 +0530

    audit fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import WhatsAppFloating from "@/components/WhatsappFloating";
 export default function Home() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen w-screen">
+      {/* <div className="flex items-center justify-center h-screen w-screen">
         <h1 className="font-bold text-2xl">
           Pay Developer Fee to resume website
         </h1>
-      </div>
-      {/* <MehndiArtistWebsite />
+      </div> */}
+      <MehndiArtistWebsite />
       <WhatsAppFloating
         phone="916287054190"
         message="Hi, I'm interested in your mehndi services. Please share availability."
-      /> */}
+      />
     </>
   );
 }
